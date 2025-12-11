21158a71d816ed49dbf7f45f9617a7a01897aa51
Andres Montoya Vargas <andresmontoyavargas.av@gmail.com>
2025-12-10 20:01:26 -0600

    Creación de modelo con Scaffold

---
+++ Models/Producto.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace TechSolutions.Models;
+
+public partial class Producto
+{
+    public string IdProd { get; set; } = null!;
+
+    public string? Nombre { get; set; }
+
+    public int? Precio { get; set; }
+
+    public int? Stock { get; set; }
+}
---
+++ Models/TechSolutionsContext.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.EntityFrameworkCore;
+
+namespace TechSolutions.Models;
+
+public partial class TechSolutionsContext : DbContext
+{
+    public TechSolutionsContext()
+    {
+    }
+
+    public TechSolutionsContext(DbContextOptions<TechSolutionsContext> options)
+        : base(options)
+    {
+    }
+
+    public virtual DbSet<Producto> Productos { get; set; }
+
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+#warning To protect potentially sensitive information in your connection string, you should move it out of source code. You can avoid scaffolding the connection string by using the Name= syntax to read it from configuration - see https://go.microsoft.com/fwlink/?linkid=2131148. For more guidance on storing connection strings, see https://go.microsoft.com/fwlink/?LinkId=723263.
+        => optionsBuilder.UseSqlServer("Server=CAMVCHAR\\SQLEXPRESS;Database=TechSolutions;Trusted_Connection=True;TrustServerCertificate=True;");
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder.Entity<Producto>(entity =>
+        {
+            entity.HasKey(e => e.IdProd).HasName("PK__Producto__B247ED70BB41314C");
+
+            entity.ToTable("Producto");
+
+            entity.Property(e => e.IdProd)
+                .HasMaxLength(50)
+                .IsUnicode(false)
+                .HasColumnName("ID_Prod");
+            entity.Property(e => e.Nombre)
+                .HasMaxLength(150)
+                .IsUnicode(false);
+        });
+
+        OnModelCreatingPartial(modelBuilder);
+    }
+
+    partial void OnModelCreatingPartial(ModelBuilder modelBuilder);
+}

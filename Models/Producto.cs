e3644f6c827374c9d6afadf3caccf598efe5c204
Andres Montoya Vargas <andresmontoyavargas.av@gmail.com>
2025-12-10 21:07:15 -0600

    Agregadas algunas validaciones al modelo

--- Models/Producto.cs
+++ Models/Producto.cs
@@ -1,15 +1,24 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 
 namespace TechSolutions.Models;
 
 public partial class Producto
 {
+    [Required(ErrorMessage = "Es necesario ingresar el ID del producto para poder registrarlo en la base de datos")]
+    [RegularExpression("^Prod-\\d+$", ErrorMessage = "El ID del producto debe ser 'Prod-' seguido del número correspondiente")]
     public string IdProd { get; set; } = null!;
 
-    public string? Nombre { get; set; }
+    [Required(ErrorMessage = "Es necesario ingresar el nombre del producto para poder registrarlo en la base de datos")]
+    [MinLength(3, ErrorMessage = "La cantidad mínima de caracteres del nombre del producto es de 3")]
+    public string? Nombre { get; set; } = string.Empty;
 
-    public int? Precio { get; set; }
+    [Required(ErrorMessage = "Es necesario ingresar el precio del producto para poder registrarlo en la base de datos")]
+    [Range(0, int.MaxValue, ErrorMessage = "El precio no puede ser negativo")]
+    public int? Precio { get; set; } = 0;
 
-    public int? Stock { get; set; }
+    [Required(ErrorMessage = "Es necesario ingresar una cantidad en stock del producto para poder registrarlo en la base de datos")]
+    [Range(0, int.MaxValue, ErrorMessage = "El precio no puede ser negativo")]
+    public int? Stock { get; set; } = 0;
 }
